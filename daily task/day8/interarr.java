87b3e23ea7e91882952a037619f93df8f04bc5fb
Tharun Mec <tharunmec.dev@gmail.com>
2026-03-24 18:58:42 +0530

    Add files via upload

---
+++ daily task/day8/interarr.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class InterSecArray{
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int[] arr1=new int[n];
+	    int[] arr2=new int[n];
+	    for(int i=0;i<n;i++){
+	        arr1[i]=sc.nextInt();
+	        arr2[i]=sc.nextInt();
+	    }
+	    for(int i=0;i<n;i++){
+	        for(int j=0;j<n;j++){
+	            if(arr1[i]==arr2[j]){
+	                System.out.print(arr1[i]+" ");
+	            }
+	        }
+	    }
+		
+	}
+}
